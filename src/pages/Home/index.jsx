4d789cde1ca388e0bc8037be929dbef02986504c
jnorberto23 <joaovictor.norberto@outlook.com>
2021-08-13 15:43:11 -0300

    Iniciando o carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
+import logo from '../../assets/logo.svg';
+import { Container, Search, Logo, Wrapper, Map, CarouselTittle } from './styles';
 
-const Home = () => <Container>Hello World</Container>;
+const Home = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="Logo" />
+          <TextField
+            outlined
+            label="Pesquisar restaurantes"
+            // onTrailingIconSelect={() => this.setState({value: ''})}
+            trailingIcon={<MaterialIcon role="button" icon="search" />}>
+            <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+          </TextField>
+          <CarouselTittle>Na sua área</CarouselTittle>
+        </Search>
+      </Container>
+      <Map/>
+    </Wrapper>
+   
+  );
+};
 
 export default Home;
--- src/pages/Home/styles.jsx
+++ src/pages/Home/styles.jsx
@@ -1,6 +1,40 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  background-color: ${(props) => props.theme.colors.background};
+export const Wrapper = styled.div`
+  display: flex;
 `;
 
+export const Container = styled.aside`
+  background-color: ${(props) => props.theme.colors.background};
+  width: 300px;
+  height: 100vh;
+  overflow-y: auto;
+`;
+
+export const Search = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff;
+  padding: 20px;
+`;
+
+export const Logo = styled.img`
+  margin-bottom: 15px;
+`;
+
+export const Map = styled.div`
+  background-color: #f01a1a;
+  width: 800px;
+  max-width: 900px;
+  height: 500px;
+`;
+
+export const CarouselTittle = styled.h1`
+  font-family: ${(props) => props.theme.fonts.regular};
+  color: ${(props) => props.theme.colors.text};
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 29px;
+  margin: 16px 0;
+`;
